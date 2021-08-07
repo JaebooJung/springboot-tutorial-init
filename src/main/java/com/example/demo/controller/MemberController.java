52d37a941e9bba53dc7e814986286d71b888abf7
jbjung <jbjung@koscom.co.kr>
2021-08-07 17:51:41 +0900

    2일차 완료

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -4,9 +4,13 @@ import com.example.demo.entity.Member;
 import com.example.demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -14,6 +18,11 @@ import java.util.List;
 @Slf4j
 public class MemberController {
     final MemberService memberService;
+
+    @GetMapping("/info")
+    public Map<String, Object> getUserInfo(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
 
     @PostMapping("/save")
     public void saveMember(@RequestBody Member member){
