7a85ff6e726fad85ceeabbb123948d6134e9542f
jbjung <jbjung@koscom.co.kr>
2021-08-07 15:53:18 +0900

    2일차 Scheduling 이전

--- src/main/java/com/example/demo/controller/FredController.java
+++ src/main/java/com/example/demo/controller/FredController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.Observation;
+import com.example.demo.service.FredService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +14,22 @@ import java.time.Duration;
 
 @RestController
 @RequestMapping("/fred")
+@RequiredArgsConstructor
 @Slf4j
 public class FredController {
+
+    final FredService fredService;
+
+    @GetMapping(path="/usbond10y")
+    public Flux<Observation> getUsBond10Y() {
+        return fredService.getUsGovernmentBond10Y();
+    }
+
+    // WebFlux 참조
+    @GetMapping(path="/usbond10y_stream", produces = MediaType.APPLICATION_NDJSON_VALUE)
+    public Flux<Observation> getUsBond10YStream() {
+        return  Flux.interval(Duration.ofMillis(1000)).zipWith(fredService.getUsGovernmentBond10Y()).map(t -> t.getT2());
+    }
 
     @GetMapping(path="/interval", produces = MediaType.APPLICATION_NDJSON_VALUE)
     public Flux<Long> interval() {
